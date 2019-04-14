93fe16ed09b22104fae284677a98c611aec5730b
Shae Erisson <shae@scannedinavian.com>
2019-04-14 16:32:36 -0400

    start of an evaluator

--- Main.hs
+++ Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
 import           Control.Applicative
@@ -41,12 +40,13 @@ data Expr = Lit Integer -- does not handle negative numbers!
 -- real lisp handles any number of args, pExpr `sepBy` char ' ' ?
 pExpr :: Parser Expr
 pExpr = Lit <$> decimal -- does not handle negative numbers
-        <|> Add <$ char '(' <* char '+' <* space <*> pExpr <* space <*> pExpr <* char ')'
-        <|> Divide <$ char '(' <* char '/' <* space <*> pExpr <* space <*> pExpr <* char ')'
-        <|> Multiply <$ char '(' <* char '*' <* space <*> pExpr <* space <*> pExpr <* char ')'
-        <|> Subtract <$ char '(' <* char '-' <* space <*> pExpr <* space <*> pExpr <* char ')'
-        <|> PLt <$ char '(' <* char '<' <* space <*> pExpr <* space <*> pExpr <* char ')'
-        <|> PGt <$ char '(' <* char '>' <* space <*> pExpr <* space <*> pExpr <* char ')'
+        -- <|> Add <$ char '(' <* char '+' <* space <*> pExpr <* space <*> pExpr <* char ')'
+        <|> Add <$ left '+' <*> pExpr <* space <*> pExpr <* char ')'
+        <|> Divide <$ left '/' <*> pExpr <* space <*> pExpr <* char ')'
+        <|> Multiply <$ left '*' <*> pExpr <* space <*> pExpr <* char ')'
+        <|> Subtract <$ left '-' <*> pExpr <* space <*> pExpr <* char ')'
+        <|> PLt <$ left '<' <*> pExpr <* space <*> pExpr <* char ')'
+        <|> PGt <$ left '>' <*> pExpr <* space <*> pExpr <* char ')'
         <|> PLtEq <$ char '(' <* string "<=" <* space <*> pExpr <* space <*> pExpr <* char ')'
         <|> PGtEq <$ char '(' <* string ">=" <* space <*> pExpr <* space <*> pExpr <* char ')'
         <|> PNumber <$ char '(' <* string "numberp" <* space <*> pExpr <* char ')'
@@ -59,9 +59,24 @@ pExpr = Lit <$> decimal -- does not handle negative numbers
             -- why doesn't this same thing work for Add, etc?
         <|> List <$ char '(' <* string "list" <*> exprs <* char ')'
             where exprs = many (space *> pExpr)
+                  left c = char '(' <* char c <* space
+
+
+pEval :: Expr -> Expr
+pEval l@(Lit _)             = l
+pEval (Add (Lit a) (Lit b)) = Lit (a + b) -- base case for add
+pEval (Add a@(Add _ _) b)   = Add (pEval a) b
+pEval (Add a b@(Add _ _))   = Add a (pEval b)
+-- pEval (Add a b) =
+-- (Add (Add a b) c) -> (Add (a + b) c)
+
+
+fromRight (Right r) = r
 
 main :: IO ()
 main = do
   print "Hello, Haskell lisp parser!"
-  print $ parseOnly pExpr "(list (+ 1 (- 2 3)))"
+  print $ parseOnly pExpr "(list (+ (- 1 1) (* 1 (- 2 3))))"
+  print "Hello, Haskell lisp evaluator!"
+  print $ Prelude.take 4 $ iterate pEval $ fromRight $ parseOnly pExpr "(+ (+ 1 1) (+ 1 1))"
 
